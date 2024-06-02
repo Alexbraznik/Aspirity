9bf44a9a12af9939592626ade80f76533594336e
Alexbraznik <Alexbraznik@gmail.com>
2024-06-02 13:06:59 +0300

    modal

--- src/components/UI/ModalUserInfoUI.jsx
+++ src/components/UI/ModalUserInfoUI.jsx
@@ -43,11 +43,12 @@ export function ModalUserInfoUI({
                   </span>
                   {field.isSelect ? (
                     <select className="bg-dark-bg-secondary border border-dark-border-primary text-sm w-full rounded px-3.5 py-2">
-                      {countries.map((el, index) => (
-                        <option key={index} value={el.value}>
-                          {el.value}
-                        </option>
-                      ))}
+                      {countries.length > 0 &&
+                        countries.map((el, index) => (
+                          <option key={index} value={el.value}>
+                            {el.value}
+                          </option>
+                        ))}
                     </select>
                   ) : (
                     <p className="bg-dark-bg-secondary border border-dark-border-primary text-sm w-full rounded px-3.5 py-2">
